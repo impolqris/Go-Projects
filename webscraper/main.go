5a6909f8823a417e916b722f5a1481c05dbb4997
impolqris <chelsdelrey@gmail.com>
2022-10-07 16:56:53 -0500

    Get all youtube IDs and the count and print it

--- webscraper/main.go
+++ webscraper/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"strings"
 
 	"github.com/chromedp/chromedp"
 )
 
 const (
-	ITPUrl         = "https://itpcareerexpo.com/pathways/aerospace-pathway/"
 	ClassToLookFor = `a[href^="https://www.youtube.com/"]`
 )
 
@@ -19,16 +18,54 @@ func main() {
 
 	var result []map[string]string
 
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(ITPUrl),
-		chromedp.AttributesAll(ClassToLookFor, &result, chromedp.ByQueryAll),
-	)
-
-	if err != nil {
-		log.Fatal(err)
+	urls := []string{
+		"https://itpcareerexpo.com/pathways/aerospace-pathway/",
+		"https://itpcareerexpo.com/pathways/agriculture-food-natural-resources-pathway/",
+		"https://itpcareerexpo.com/pathways/architecture-construction-pathway/",
+		"https://itpcareerexpo.com/pathways/arts-audio-visual-technology-communications-pathway/",
+		"https://itpcareerexpo.com/pathways/business-management-administration-pathway/",
+		"https://itpcareerexpo.com/pathways/education-training-pathway/",
+		"https://itpcareerexpo.com/pathways/energy-pathway/",
+		"https://itpcareerexpo.com/pathways/engineering-pathway/",
+		"https://itpcareerexpo.com/pathways/finance-pathway/",
+		"https://itpcareerexpo.com/pathways/government-public-administration-pathway/",
+		"https://itpcareerexpo.com/pathways/health-sciences-pathway/",
+		"https://itpcareerexpo.com/pathways/hospitality-tourism-pathway/",
+		"https://itpcareerexpo.com/pathways/human-services-pathway/",
+		"https://itpcareerexpo.com/pathways/information-technology-pathway/",
+		"https://itpcareerexpo.com/pathways/law-public-safety-corrections-security-pathway/",
+		"https://itpcareerexpo.com/pathways/manufacturing-pathway/",
+		"https://itpcareerexpo.com/pathways/marketing-pathway/",
+		"https://itpcareerexpo.com/pathways/transportation-distribution-logistics-pathway/",
 	}
+
+	for _, v := range urls {
+		err := chromedp.Run(ctx,
+			chromedp.Navigate(v),
+			chromedp.AttributesAll(ClassToLookFor, &result, chromedp.ByQueryAll),
+		)
+
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+	}
+
+	fullUrls := []string{}
 
 	for _, v := range result {
-		fmt.Printf("%v\n", v)
+		for _, s := range v {
+			if strings.Contains(s, "https://www.youtube.com") {
+				fullUrls = append(fullUrls, s)
+			}
+		}
 	}
+
+	ytIds := []string{}
+
+	for _, ux := range fullUrls {
+		ytIds = append(ytIds, ux[32:])
+	}
+
+	fmt.Printf("%v\n", ytIds)
+	fmt.Printf("%d\n", len(ytIds))
 }
